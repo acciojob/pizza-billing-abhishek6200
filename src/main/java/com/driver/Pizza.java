9a1dca4739408854c92eeab10e29c5741666bffa
Your Name <you@example.com>
2023-09-16 19:41:54 +0530

    first attempt

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,14 +1,25 @@
 package com.driver;
 
+import java.net.StandardSocketOptions;
+
 public class Pizza {
 
     private int price;
     private Boolean isVeg;
     private String bill;
+    private boolean extraCheese;
+    private boolean extraToppings;
+    private int extraToppingsCost;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        if(this.isVeg == true){
+           System.out.println("Base Price Of The Pizza: " + (price += 300));
+            this.extraToppingsCost = 70;
+        }else{
+            System.out.println("Base Price Of The Pizza: " + (price += 400));
+            this.extraToppingsCost = 120;
+        }
     }
 
     public int getPrice(){
@@ -16,19 +27,32 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+       if(extraCheese == false){
+           System.out.println("Extra Cheese Added: " + 80);
+           price += 80;
+           extraCheese = true;
+       }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(extraToppings == false){
+            System.out.println("Extra Toppings Added: " + extraToppingsCost);
+            price += extraToppingsCost;
+            extraToppings = true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        System.out.println("Paperbag Added: " + 20);
+        price += 20;
     }
 
     public String getBill(){
         // your code goes here
+        this.bill = String.valueOf(price);
+        System.out.print("Total Price: ");
         return this.bill;
     }
 }
